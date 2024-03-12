fe651553d1c6086e5bc7ad5c97e5fb8ac7b311d0
VitorKokkonen <vitorks08@outlook.com>
2024-03-12 16:26:20 -0300

    Adicionando arquivos

---
+++ components/HomeScreen/index.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+function HomeScreen() {
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text>Resenha Street Bar</Text>
+        </View>
+    );
+}
+
+export default HomeScreen;
